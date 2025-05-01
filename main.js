28ffb9b9b1525f82185b41af6060c5f344ea814a
Ismael Cruz <ibcruzdeveloper@gmail.com>
2025-05-02 01:56:16 +0200

    holaMundo

---
+++ main.js
@@ -0,0 +1,19 @@
+
+
+
+
+let mes = 8;
+let estacion;
+
+if(mes == 1 || 2 || 12){
+    console.log("Es Invierno");
+}
+else if(mes == 3 || 4 || 5){
+    console.log("Es Primavera");
+}
+else if(mes == 6 || 7 || 8){
+    console.log("Es Verano");
+}
+else if(mes == 9 || 10 || 11){
+    console.log("Es Otoño");
+}
