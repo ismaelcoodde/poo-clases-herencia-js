18ffdb002fbc187165d96ca40b4d09e7b1d002ad
Ismael Cruz <ibcruzdeveloper@gmail.com>
2025-05-02 02:19:04 +0200

    Bucle While

---
+++ while.js
@@ -0,0 +1,10 @@
+// Ciclo While
+
+// Mientras el contador sea menor de 10 se ira repitiendo y incrementando
+
+let contador = 3;
+
+while(contador < 10){
+    console.log(contador)
+    contador++;
+}
