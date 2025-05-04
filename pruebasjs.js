6509d52dd565bea177d3c8ce9cc5c791890ad185
Ismael Cruz <ibcruzdeveloper@gmail.com>
2025-05-04 02:26:57 +0200

    objetos con propiedades y dentro hago una funcion

--- pruebasjs.js
+++ pruebasjs.js
@@ -1,8 +1,11 @@
-// Aqui lo que hago es en un ciclo for usar un break para que a la primera que se cumpla se rompa
-
-for(let contador = 0; contador <= 10; contador++){
-    if(contador % 2 == 0){
-        console.log(contador)
-        break;
+let persona = {
+    nombre: 'Ismael',
+    apellidos: 'Cruz Fernández',
+    edad: 30,
+    nombreCompleto: function(){
+        return this.nombre + " " + this.apellidos;
     }
 }
+console.log(persona.nombreCompleto());
+
+// Esto es un objeto que dentro haces una función
