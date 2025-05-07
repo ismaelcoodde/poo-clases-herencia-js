7bed4f147468bab364ce34281db6b0f8e0ff5e9e
Ismael Cruz <ibcruzdeveloper@gmail.com>
2025-05-08 01:55:06 +0200

    funcion dentro de objeto constructor

--- pruebasjs.js
+++ pruebasjs.js
@@ -1,10 +1,19 @@
-// Funcion constructor de objetos de tipo Persona
-// (funcion constructor para crear un objeto de tipo persona que va a tener 3 atributos)
-function Persona(nombre, apellido, email){ //<-- Parametros
+
+
+//metiendo una funcion dentro del constructor de objetos
+
+
+
+
+function Persona(nombre, apellido, email){ 
     this.nombre = nombre;
     this.apellido = apellido;
     this.email = email;
+    this.nombreCompleto = function(){
+        return this.nombre + ' ' + this.apellido;
+    }
 }
 let padre = new Persona('Ismael', 'Cruz', 'ibcruz@gmail.com');
 let madre = new Persona('Antomia', 'Maria','taty@gmail.com')
-console.log(padre, madre)
+console.log(padre)
+console.log(padre.nombreCompleto());
