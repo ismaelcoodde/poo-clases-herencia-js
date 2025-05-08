6cd4ac5dd3b6d2f9c4ff90b47dc65ffcb78eee41
Ismael Cruz <ibcruzdeveloper@gmail.com>
2025-05-08 02:30:44 +0200

    call

--- pruebasjs.js
+++ pruebasjs.js
@@ -1,21 +1,20 @@
-
-
 //añado nuevos valores y propiedades
 
+let persona1 = {
+  nombre: "Ismael",
+  apellido: "Cruz",
+  nombreCompleto: function () {
+    return this.nombre + " " + this.apellido;
+  },
+};
 
+let persona2 = {
+    nombre: "Juan",
+    apellido: "Ferrer",
+  };
+  
+console.log(persona1.nombreCompleto());
 
+console.log(persona1.nombreCompleto.call(persona2));
 
-function Persona(nombre, apellido, email){ 
-    this.nombre = nombre;
-    this.apellido = apellido;
-    this.email = email;
-    this.nombreCompleto = function(){
-        return this.nombre + ' ' + this.apellido;
-    }
-}
-Persona.prototype.tel = 234234;
-let padre = new Persona('Ismael', 'Cruz', 'ibcruz@gmail.com');
-let madre = new Persona('Antomia', 'Maria','taty@gmail.com')
-madre.tel = 616831983
-console.log(padre.tel)
-console.log(madre.tel);
+// Uso de call para usar el metodo persona1.nombreCompleto con los datos del objeto persona2
